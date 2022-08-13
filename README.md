dd61bfe9b9c2cf49f75e8821153c3d18772effa4
mihoadiego <hoareaum@gmail.com>
2022-08-13 11:56:04 +0200

    setting connections and first listeners when joining a room

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # realtime_chat_app_with_rooms_back
 HarperDB+Express+SocketIO
+## dependancies
+Repository realtime_chat_app_with_rooms_front
+## npm steps that led to project
+npm init -y ## 
+npm i axios cors express socket.io dotenv ##
+npm i -D nodemon ##
+adding then into package.json the following mentions
+"scripts": {
+    "dev": "nodemon index.js",
+}
+### run application after
+npm run dev 
+Served on port 4000
--- index.js
+++ index.js
@@ -1,12 +1,75 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+
+// Add cors middleware
 const cors = require('cors');
+app.use(cors()); 
 
-app.use(cors()); // Add cors middleware
 
+// set SocketIO connection, integrating cors management, and setting variables that will help us managing events/communications/rooms..
+const { Server } = require('socket.io');
 const server = http.createServer(app);
-app.get('/', (req, res) => {
-    res.send('Hello you');
+const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST'],
+    },
   });
+const CHATBOT = 'chatBot';
+let chatRoom = '';
+let allMembers =[];
+
+
+/**
+ * =====================================================================================================================================
+ * LISTENER TO  FRONTEND CONNECTIONS    
+ *    dependancies:   GitHub REPOSITORY  named realtime_chat_app_with_rooms_front/src/App.js
+ *                    below listener INDEED listens to every socketIO connexion coming from the front End
+ *                    realtime_chat_app_with_rooms_front REPOSITORY, 
+ *                     => file src/App.js directly connects through const socket = io.connect('http://localhost:4000') ) 
+ * =====================================================================================================================================  
+ */ 
+io.on('connection', (socket) => {    
+    // Associated logic and sub listeners (coming from socket) here (adding user to rooms...)
+    console.log(`User connected ${socket.id}`);  
+
+    
+
+    //Adding to a room 
+    socket.on('join_room', (data) => {
+        // Data sent from FRONTEND when join_room event emitted
+        const { username, room } = data;
+
+        // Join the user to a socket room 
+        socket.join(room); 
+
+        let __createdtime__ = Date.now(); 
+        
+        // Send message to users currently in room, apart from the user that just joined
+        socket.to(room).emit('receive_message', {
+            message: `${username} has joined the chat room`,
+            username: CHATBOT,
+            __createdtime__,
+        });
+
+        // Send welcome msg to user that just joined chat only
+        socket.emit('receive_message', {
+            message: `Welcome ${username}`,
+            username: CHATBOT,
+            __createdtime__,
+        });
+
+        chatRoom = room;
+        // add the new connected user to the list of all connected members
+        allMembers.push({ id: socket.id, username, room }); 
+        // filter users belonging to the room where the new connected user is trying to join
+        chatRoomUsers = allMembers.filter((member) => member.room === room); 
+        socket.to(room).emit('chatroom_users', chatRoomUsers);
+        socket.emit('chatroom_users', chatRoomUsers);
+
+      });
+
+});
+
 server.listen(4000, () => 'Server running on port 4000');
